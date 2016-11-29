b365164e562b5657702d1ec88773543f0a27d223
FanHoo <308289197@qq.com>
2016-11-29 17:11:29 +0800

    add

---
+++ app/src/main/java/fanhoo/com/projectbase/base/interfaces/OnItemClickListener.java
@@ -0,0 +1,13 @@
+package fanhoo.com.projectbase.base.interfaces;
+
+import android.view.View;
+
+/**
+ * 创建人 胡焕
+ * 创建时间 2016年11月28日  09:36
+ * 用途
+ */
+
+public interface OnItemClickListener {
+    public void onItemClick(View v, int position);
+}
